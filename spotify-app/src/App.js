69f8a6c668037d78b12168c30d6af0a1ce12fb06
Colin Malcolm <colinmalcolm8@gmail.com>
2020-09-06 18:33:45 -0400

    finished cards styling

--- spotify-app/src/App.js
+++ spotify-app/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.scss';
 import Nav from './Components/Nav'
-
+import Main from './Components/Main'
 
 function App() {
   return (
     <div className="outerWrap">
       <div className="App">
         <Nav />
+        <Main />
       </div>
       <div className="musicControl">
         music controls
---
+++ spotify-app/src/Components/Main.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Main = () => {
+    return <div className="main">
+        <div className="upperNav">
+            dummy text
+        </div>
+
+        <div className="mainContent">
+            <h1> Uniquely Yours </h1>
+            <div className="cardsWrap">
+                <div className="card">
+                    <div className="cardImage">
+                        <img src="https://images.unsplash.com/photo-1470229722913-7c0e2dbbafd3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt="test 1"/>
+                    </div>
+                    <div className="cardContent">
+                        <h3>Liked Songs</h3>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
+    </div>
+}
+
+export default Main
